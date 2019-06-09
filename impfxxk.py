e1fea63335b72afb45fd9b488d1c1d7b6934fbad
Jarry Shaw <JarryShaw@users.noreply.github.com>
2019-06-09 21:31:38 +0800

    Start implementing

--- impfxxk.py
+++ impfxxk.py
@@ -1,50 +1,50 @@
 # -*- coding: utf-8 -*-
 
-import importlib.machinery
+import importlib.abc
+import importlib.util
 import sys
 
+###############################################################################
+# See: importlib.machinery.PathFinder
 
-class ImpFxxkPathFinder(importlib.machinery.PathFinder):
+class ImpFxxkPathFinder(importlib.abc.MetaPathFinder):
 
-    @classmethod
-    def find_spec(cls, fullname, path, target=None):
-        print(fullname, path, target)
-        super().find_spec(fullname, path, target)
-
-
-class ImpFxxkFileFinder(importlib.machinery.FileFinder):
-
-    def __init__(self, path, *loader_details):
-        print(path, loader_details)
-        super().__init__(path, *loader_details)
-
-    def find_spec(self, fullname, target=None):
-        print(fullname, target)
-        super().find_spec(fullname, target)
-
-    @classmethod
-    def path_hook(cls, *loader_details):
-        def path_hook_for_FileFinder(path):
-            """Path hook for importlib.machinery.FileFinder."""
-            print(path)
-            if not importlib.machinery._path_isdir(path):
-                raise ImportError('only directories are supported', path=path)
-            return cls(path, *loader_details)
-
-        return path_hook_for_FileFinder
+    pass
 
 
-loader_details = (importlib.machinery.SourceFileLoader,
+###############################################################################
+# See: importlib.machinery.FileFinder
+
+
+class ImpFxxkFileFinder(importlib.abc.PathEntryFinder):
+
+    pass
+
+
+###############################################################################
+# See: importlib.machinery.SourceFileLoader
+
+
+class ImpFxxkSourceLoader(importlib.abc.SourceLoader):
+
+    pass
+
+
+###############################################################################
+# Setting up impfxxk.
+
+# Setting up loader details.
+loader_details = (ImpFxxkSourceLoader,
                   importlib.machinery.SOURCE_SUFFIXES)
 
-# register sys.meta_path
-sys.meta_path.insert(0, ImpFxxkPathFinder())
+# Setting up a meta path finder.
+sys.meta_path.append(ImpFxxkPathFinder())
 
-# register sys.path_hooks
-sys.path_hooks.insert(0, ImpFxxkFileFinder.path_hook(loader_details))
+# Setting up a path entry finder.
+sys.path_hooks.append(ImpFxxkFileFinder.path_hook(loader_details))
 
-# clear out the internal cache
-importlib.invalidate_caches()
-
-# cleanup __main__ cache
-# sys.modules.pop('__main__')
+# Setting up __main__.__spec__
+__main__ = sys.modules['__main__']
+if __main__.__spec__ is None:
+    __main__.__spec__ = importlib.util.spec_from_loader(__main__.__name__, ImpFxxkPathFinder,
+                                                        origin=__main__.__file__)
